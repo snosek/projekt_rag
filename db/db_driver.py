7a9ee40082a1af3e6ebfa89610f985d2e28ec4f2
Stefan Nosek <stefan.nosek1@gmail.com>
2025-06-16 00:21:03 +0200

    db works and populated

---
+++ db/db_driver.py
@@ -0,0 +1,36 @@
+from os import getenv
+from contextlib import contextmanager
+from dotenv import load_dotenv
+from sqlalchemy import URL, create_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+conn_url = URL.create(
+    drivername="postgresql+psycopg2",
+    username=getenv("POSTGRES_USER"),
+    password=getenv("POSTGRES_PASSWORD"),
+    host="localhost",
+    port=getenv("POSTGRES_PORT"),
+    database=getenv("POSTGRES_DB"),
+)
+engine = create_engine(conn_url)
+session = sessionmaker(bind=engine, autoflush=False)
+
+@contextmanager
+def get_db(commit: bool = False):
+    """
+    Usage:
+        >>> with get_db() as db:
+        ...     version = db.execute(text("SELECT version();")).all()
+    """
+    db = session()
+    try:
+        yield db
+    except Exception as e:
+        db.rollback()
+        raise e
+    finally:
+        if commit:
+            db.commit()
+        db.close()
---
+++ db/populate_db.py
@@ -0,0 +1,59 @@
+import argparse
+import json
+from pathlib import Path
+from sqlalchemy import text
+from db_driver import get_db
+
+def main():
+    args = parse_args()
+    if args.init:
+        create_schema()
+    if args.populate:
+        populate_db()
+
+def create_schema():
+    print("running create_schema")
+    with get_db(commit=True) as db:
+        db.execute(text("DROP TABLE IF EXISTS course_data;"))
+        db.execute(text("CREATE EXTENSION IF NOT EXISTS vector;"))
+        db.execute(
+            text("""CREATE TABLE IF NOT EXISTS course_data (
+                id SERIAL PRIMARY KEY,
+                data JSONB,
+                embedding VECTOR(384)
+            );""")
+        )
+
+def populate_db():
+    print("running populate_db")
+    courses_dir = "data/dane_low_level"
+    courses_dir = Path("data/dane_low_level")
+    json_filepaths = courses_dir.glob("*.json")
+    for filepath in json_filepaths:
+        with open(filepath, "r", encoding="utf-8") as f:
+            try:
+                data = json.dumps(json.load(f))
+            except json.JSONDecodeError as e:
+                print(f"Failed to load {filepath}: {e}")
+                continue
+        with get_db(commit=True) as db:
+            db.execute(
+                text("INSERT INTO course_data (data) VALUES (:data)"), {"data": data}
+            )
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--init", 
+        action=argparse.BooleanOptionalAction,
+        help="whether to init db schema",
+    )
+    parser.add_argument(
+        "--populate", 
+        action=argparse.BooleanOptionalAction,
+        help="whether to populate db with course data",
+    )
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    main()
